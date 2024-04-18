481991394f165ecd1247e59d60ab394edc7367d9
syedhamzasiff <syedhamzasiff@gmail.com>
2024-04-18 16:57:15 +0500

    middleware and optimisations

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -5,7 +5,7 @@ import { getDataFromToken } from "@/helpers/getDataFromToken";
 
 connect()
 
-export async function POST(request: NextRequest){
+export async function GET(request: NextRequest){
     try{
         //extract data from token
         const userId = await getDataFromToken(request);
